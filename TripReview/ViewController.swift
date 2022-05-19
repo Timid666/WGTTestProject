a91887ff12c29b5f719fccbb0990f474d3436380
Said <said_saidov_1999@mail.ru>
2022-05-19 22:19:51 +0300

    Initial Commit

---
+++ TripReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripReview
+//
+//  Created by Саид on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
